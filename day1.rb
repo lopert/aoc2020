a625710264c1e8c3d051cbf6acffd3b898d8bf1a
Alex Bradley <alexandre.bradley@shopify.com>
2020-12-02 21:31:26 -0500

    reorg

--- day1.rb
+++ day1.rb
@@ -1,29 +1,29 @@
 # https://adventofcode.com/2020/day/1
 
-# --- Day 1: Report Repair ---
-# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-
-# For example, suppose your expense report contained the following:
-
-# 1721
-# 979
-# 366
-# 299
-# 675
-# 1456
-# In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-
-# Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
-
 class ExpenseReader
 
     def initialize(filename)
         @filename = filename
     end
 
-    def solve
+    # --- Day 1: Report Repair ---
+    # Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+
+    # For example, suppose your expense report contained the following:
+
+    # 1721
+    # 979
+    # 366
+    # 299
+    # 675
+    # 1456
+    # In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+    # Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+    def solve_part_one
         file_data = File.read(@filename).split
 
+        #
         under_one_thousand = []
 
         file_data.each_with_index do |number, index|
@@ -47,7 +47,17 @@ class ExpenseReader
 
     end
 
+    # --- Part Two ---
+    # The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+    # Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+    # In your expense report, what is the product of the three entries that sum to 2020?
+    def solve_part_two
+
+    end
+
 end
 
 solver = ExpenseReader.new("day1input.txt")
-puts solver.solve
+puts solver.solve_part_one
