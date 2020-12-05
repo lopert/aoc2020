4d1e9049e7699df504e7be669b95227af277941b
Alex Bradley <alexandre.bradley@shopify.com>
2020-12-04 22:32:00 -0500

    part2, not correct somehow?

--- day4.rb
+++ day4.rb
@@ -30,20 +30,86 @@ class PassportChecker
 
     def count_valid_passports
         count = 0
-        required_keys = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
         @passports.each do |passport|
-            count += 1 if validate_passport(passport, required_keys)
+            count += 1 if validate_passport(passport)
         end
         puts count
     end
 
-    def validate_passport(passport, required_keys)
+    def validate_passport(passport)
+        required_keys = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
         required_keys.each do |key|
             return false unless passport.key?(key.to_sym)
         end
         return true
     end
 
+end
+
+class PassportCheckerV2 < PassportChecker
+    def validate_passport(passport)
+
+        # pp passport
+
+        # validate presence
+        required_keys = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+        required_keys.each do |key|
+            return false unless passport.key?(key.to_sym)
+        end
+
+        # pp "keys validated"
+
+        return false unless (passport[:byr].to_i >= 1920) and (passport[:byr].to_i <= 2002)
+        return false unless (passport[:iyr].to_i >= 2010) and (passport[:iyr].to_i <= 2020)
+        return false unless (passport[:eyr].to_i >= 2020) and (passport[:eyr].to_i <= 2030)
+
+        # pp "numeric checks passed"
+        return false unless validate_height(passport[:hgt])
+        return false unless validate_hair_color(passport[:hcl])
+
+        # pp "half of custom checks passed"
+        return false unless validate_eye_color(passport[:ecl])
+
+        # pp "only passport number remains!"
+        return false unless validate_passport_number(passport[:pid])
+
+        # pp "all checks passed"
+
+        # pp passport
+
+        return true
+
+    end
+
+    def validate_height(hgt)
+        unit = hgt[-2..-1]
+        dimension = hgt[0..-3].to_i
+
+        if unit == "cm"
+            return false unless ((dimension >= 150) or (dimension <= 193))
+        elsif unit == "in"
+            return false unless ((dimension >= 59) or (dimension <= 76))
+        else
+            return false
+        end
+
+        return true
+    end
+
+    def validate_hair_color(hcl)
+        return false unless hcl.length == 7
+        return false unless hcl[0] == "#"
+        return !hcl[1..-1][/\H/] #https://stackoverflow.com/questions/20403636/how-to-validate-that-a-string-is-a-proper-hexadecimal-value-in-ruby
+    end
+
+    def validate_eye_color(ecl)
+        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(ecl)
+    end
+
+    def validate_passport_number(pid)
+        return false unless pid.length == 9
+        return !!pid[/\d{9}/]
+    end
 
 end
 
@@ -51,3 +117,7 @@ pp "PART 1"
 
 solver = PassportChecker.new()
 solver.solve
+
+pp "PART 2"
+solver2 = PassportCheckerV2.new()
+solver2.solve
