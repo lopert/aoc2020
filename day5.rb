683a84b88340238bffad1f2d09b8885c690d2943
Alex Bradley <alexandre.bradley@shopify.com>
2020-12-05 09:20:16 -0500

    day5part2

--- day5.rb
+++ day5.rb
@@ -44,5 +44,40 @@ class BoardingPassDecoder
 
 end
 
+class BoardingPassFinder < BoardingPassDecoder
+    def solve
+        all_ids = []
+        @passes.each do |pass|
+            all_ids << decode(pass)[:id]
+        end
+        
+        pp find_missing_id(all_ids)
+    end
+
+    def find_missing_id(ids)
+        ids = ids.sort
+        missing_ids = []
+        ids.each_with_index do |id, index|
+            prev_id = ids[index-1]
+
+            # avoid the first id - there must be a better way to do this
+            next if prev_id == ids[-1]
+
+            # pp "Comparing #{id} to #{prev_id} + 1... #{id == (prev_id+1)}"
+            return id - 1 unless id == (prev_id+1)
+
+        end
+
+        missing_ids
+    end
+
+end
+
+puts "Part One"
 solver = BoardingPassDecoder.new()
 solver.solve
+
+puts "Part Two"
+solver2 = BoardingPassFinder.new()
+solver2.solve
+# 563 too high
