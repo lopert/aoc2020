bd0c0ceed261996e54bfcd8fe03466cca602810c
Alex Bradley <alexandre.bradley@shopify.com>
2021-01-04 11:32:41 -0500

    day6part2

--- day6.rb
+++ day6.rb
@@ -29,5 +29,16 @@ class FormAnalyzer
 
 end
 
+class FormAnalyzerV2 < FormAnalyzer
+    def group_yes(group_data)
+        group_data.split("\n").inject do |result, person_data|
+            (result.split('') & person_data.split('')).join
+        end.length
+    end
+end
+
 solver = FormAnalyzer.new()
 solver.solve
+
+solver_two = FormAnalyzerV2.new()
+solver_two.solve
