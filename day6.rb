efc44a0ae2f3df23ae97b7d359bb3581a8cd206f
Alex Bradley <alexandre.bradley@shopify.com>
2021-01-04 11:17:16 -0500

    day6part1

---
+++ day6.rb
@@ -0,0 +1,33 @@
+class FormAnalyzer
+
+    def initialize
+        @data_by_group = File.read("day6input.txt").split("\n\n")
+    end
+
+    def solve
+
+        result = 0
+        @data_by_group.each do |group_data|
+            result += group_yes(group_data)
+        end
+
+        puts result
+
+    end
+
+    def group_yes(group_data)
+        
+        yes_values = ""
+
+        group_data.split("\n").each do |person_data|
+            yes_values << person_data
+        end
+
+        yes_values.chars.uniq.count
+
+    end
+
+end
+
+solver = FormAnalyzer.new()
+solver.solve
