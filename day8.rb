f1bb908aafef831e2fc3117843a546065061d7a1
Alex Bradley <alexandre.bradley@shopify.com>
2021-01-05 14:55:38 -0500

    day8part2

--- day8.rb
+++ day8.rb
@@ -5,26 +5,18 @@ class GameConsole
         @instructions = File.read("day8input.txt").split("\n")
         @current_line = 0
         @executed_lines = []
+        @fix_line = -1
     end
 
-    def boot
-
-        until jackout?(@current_line)
-            @executed_lines << @current_line
-
-            ins = parse_instruction(@instructions[@current_line])
-
-            puts "Executing #{@current_line}: #{ins[:instruction]} #{ins[:num]}"
-
-            self.send(ins[:instruction],ins[:num])
-        end
-
-        puts "Infinite Loop detected. Accumulator value: #{@accumulator}"
-
+    def reset
+        @accumulator = 0
+        @instructions = File.read("day8input.txt").split("\n")
+        @current_line = 0
+        @executed_lines = []
     end
 
     def jackout?(line_number)
-        @executed_lines.include?(line_number)
+        @executed_lines.include?(line_number) || line_number >= @instructions.length
     end
 
     def acc(num)
@@ -49,6 +41,48 @@ class GameConsole
         num = line.split(" ")[1].delete('+')
 
         {instruction: instruction, num: num.to_i}
+    end
+ 
+    def boot
+
+        until jackout?(@current_line)
+            @executed_lines << @current_line
+
+            ins = parse_instruction(@instructions[@current_line])
+
+            # puts "Executing #{@current_line}: #{ins[:instruction]} #{ins[:num]}"
+
+            self.send(ins[:instruction],ins[:num])
+        end
+
+        if @current_line >= @instructions.length
+            ## good
+            puts "Program terminated with no errors. Acc value: #{@accumulator}"
+        else
+            puts "Infinite Loop detected. Accumulator value: #{@accumulator}"
+            uncorrupt
+        end
+
+    end
+
+    def uncorrupt
+       
+        # puts "Re-Initializing..."
+        self.reset
+
+        @fix_line += 1
+        # puts "Attempting to fix line #{@fix_line}"
+
+        if @instructions[@fix_line].include?("nop") 
+            @instructions[@fix_line] = "jmp" + @instructions[@fix_line][3..-1]
+            boot
+        elsif @instructions[@fix_line].include?("jmp")
+            @instructions[@fix_line] = "nop" + @instructions[@fix_line][3..-1]
+            boot
+        else
+            uncorrupt
+        end
+
     end
 end
 
