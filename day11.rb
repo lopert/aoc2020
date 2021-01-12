39a70732aad179e8820185ff8319ff81e15671db
Alex Bradley <alexandre.bradley@shopify.com>
2021-01-12 17:00:28 -0500

    day11part2

--- day11.rb
+++ day11.rb
@@ -5,15 +5,30 @@ class EncodingError
     end
 
     def solve
-        
         @number_output.each_cons(@pre_length+1) do |array|
-
             number = array.pop
             return number unless array.combination(2).detect { |a,b| a+b == number}
-
         end
+    end
+end
+
+class EncodingErrorV2 < EncodingError
+    def solve
+        error = super
+        sliding_window = []
+
+        @number_output.each do |num|
+            sliding_window << num
+            sliding_window.shift while sliding_window.sum > error
+            return (sliding_window.min + sliding_window.max) if sliding_window.sum == error
+        end
+
+        return "No solution found"
     end
 end
 
 solver = EncodingError.new
 puts "Part 1: #{solver.solve}"
+
+solver = EncodingErrorV2.new
+puts "Part 2: #{solver.solve}"
