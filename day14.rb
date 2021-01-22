450e803d01c77f25c3cecbdc10f5674a02078dca
Alex Bradley <alexandre.bradley@shopify.com>
2021-01-22 01:27:39 -0500

    refactor

--- day14.rb
+++ day14.rb
@@ -11,21 +11,23 @@ class DockingData
             parse_instruction(line)
         end
 
-        p @mem.values.inject(:+)
+        @mem.values.inject(:+)
 
     end
 
     def parse_instruction(line)
-
-        type = line.split(" = ")[0]
-        value = line.split(" = ")[1]
+        type, value = line.split(" = ")
 
         if type.include?("mask")
             @mask = value
         else
-            address = type.split("[")[1][0..-2]
-            @mem[address] = apply_mask(value)
+            parse_memory_instruction(type, value)
         end
+    end
+
+    def parse_memory_instruction(type, value)
+        address = type.split("[")[1][0..-2]
+        @mem[address] = apply_mask(value)
     end
 
     def apply_mask(num)
@@ -46,24 +48,15 @@ end
 
 class DockingDataV2 < DockingData
 
-    def parse_instruction(line)
+    def parse_memory_instruction(type, value)
+        address = type.split("[")[1][0..-2]
 
-        type = line.split(" = ")[0]
-        value = line.split(" = ")[1]
-
-        if type.include?("mask")
-            @mask = value
-        else
-            address = type.split("[")[1][0..-2]
-
-            masked_addresses = apply_mask(address)
-            # puts "Mask leads to #{masked_addresses.length} addresses"
-            # puts "Applying value of #{value}"
-            masked_addresses.each do |address|
-                @mem[address] = value.to_i
-            end
+        masked_addresses = apply_mask(address)
+        # puts "Mask leads to #{masked_addresses.length} addresses"
+        # puts "Applying value of #{value}"
+        masked_addresses.each do |address|
+            @mem[address] = value.to_i
         end
-    
     end
 
     def apply_mask(num)
@@ -79,7 +72,7 @@ class DockingDataV2 < DockingData
             floating_indexes << index if mask == "X"
         end
 
-        # create array of addresses to be changed
+        # create array of addresses to be expanded
         result = Array.new(2**floating_indexes.length) {bin_num.dup} #wtf why do I need .dup here, strings are shallow?!
 
         # treat all Xs as a binary number, to be incremented as we iterate over addresses
@@ -87,8 +80,8 @@ class DockingDataV2 < DockingData
 
         # iterate over every address and sub X for 0s and 1s
         result.each do |num|
-            floating_indexes.each_with_index do |floating_index, counter|
-                num[floating_index] = current_float_bin[counter]
+            floating_indexes.each_with_index do |char_index, float_bin_position|
+                num[char_index] = current_float_bin[float_bin_position]
             end
             
             # increment binary number
@@ -96,16 +89,14 @@ class DockingDataV2 < DockingData
         end
 
         # return decimal list of addresses to modify
-        result.map do |e|
-            e.to_i(2)
-        end
+        result.map {|e| e.to_i(2)}
     end
 
 
 end
 
 solver = DockingData.new
-solver.solve
+puts solver.solve #== 10050490168421
 
 solver_two = DockingDataV2.new
-solver_two.solve
+puts solver_two.solve #== 2173858456958
